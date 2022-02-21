f255048283951fc7fdf78db4d489f757d9e8877c
PK7999246822 <priyankakhatri1202@gmail.com>
2022-02-21 19:10:42 +0530

    priya

---
+++ src/routes/logger.js
@@ -0,0 +1,9 @@
+let url = 'https;//www.google.com'
+
+function abc(message){
+    console.log(message)
+
+}
+module.exports.endpoint=url
+
+module.exports.printMyMessage=abc
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,11 @@
+const obj = require("./logger")
 const express = require('express');
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
+    obj.printMyMessage('thorium')
+    console.log(obj.endpoint)
 });
 
 module.exports = router;
