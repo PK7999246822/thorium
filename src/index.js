7ddd039368a04b22c36808a0166c1bad5be0d349
PK7999246822 <priyankakhatri1202@gmail.com>
2022-03-03 03:19:11 +0530

    2 march populate

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://priyanka-1234:xiadnNJjsOdWwU2i@cluster0.yf3ho.mongodb.net/priyanka-1234?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
