dece1878c46b4be534785e69881c4b720d61e5cb
PK7999246822 <priyankakhatri1202@gmail.com>
2022-03-12 23:52:43 +0530

    axious/vaccination/promises/memes

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -78,8 +78,99 @@ let getOtp = async function (req, res) {
     }
 }
 
+let getDistrictsid = async function (req, res) {
+    try {
+        let id = req.query.districtsid
+        let date=req.query.date
+        console.log(`query params are: ${id} ${date}`)
+        let options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${id}&date=${date}`
+        }
+        let result = await axios(options);
+        console.log(result)
+        let data = result.data
+        res.status(200).send({ msg: data, status: true })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+const getLondonWeather = async function (req, res) {
+
+    let London = req.query.q
+    let ad22e4379c250c79624362ee10bb5fd2 = req.query.appid
+
+    let options = {
+        method: "get",
+        url: `http://api.openweathermap.org/data/2.5/weather?q=${London}&appid=${ad22e4379c250c79624362ee10bb5fd2}`
+    }
+
+    let result = await axios(options)
+    //console.log(result)
+    let data = result.data
+    //console.log(data)
+    res.status(201).send({ status: true, msg: data })
+
+    let temperature=data.main.temp;
+    console.log(temperature)
+}
+
+let getWeather = async function (req, res) {
+    try {
+    
+        let cities = ["Bengaluru","Pune","Delhi","Hyderabad","London"];
+        let ObjCities=[];
+        
+        for(i=0; i<cities.length; i++){
+     let obj={city:cities[i]}
+        let result = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cities[i]}&appid=5c5ed96b9361a5c37efcbaee6a24fe8f`)
+        console.log(result.data.main.temp)
+        obj.temp=result.data.main.temp
+        ObjCities.push(obj)
+        }
+        let sorted =ObjCities.sort( function(a, b) {return a.temp - b.temp})
+        console.log(sorted)
+        res.status(200).send({status: true, data:sorted})
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+   
+const memes = async function (req, res) {
+    try {
+        let id = req.query.template_id
+        let text0 = req.query.text0
+        let text1 = req.query.text1
+        let username = req.query.username
+        let password = req.query.password
+        let options = {
+            method: "post",
+            url: `https://api.imgflip.com/caption_image?text0=${text0}&text1=${text1}&template_id=${id}&username=${username}&password=${password}`,
+            // data: data
+        }
+        let result = await axios(options)
+        console.log(result.data)
+        res.status(200).send({ msg: result.data })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+    
+    
 
 module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+module.exports.getDistrictsid =getDistrictsid
+module.exports.getLondonWeather=getLondonWeather
+module.exports.getWeather=getWeather
+module.exports.memes=memes
--- src/routes/route.js
+++ src/routes/route.js
@@ -12,11 +12,13 @@ router.get("/test-me", function (req, res) {
 router.get("/cowin/states", CowinController.getStates)
 router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
 router.get("/cowin/getByPin", CowinController.getByPin)
+router.get("/cowin/getDistrictsid", CowinController.getDistrictsid)
 
 router.post("/cowin/getOtp", CowinController.getOtp)
+router.get("/cowin/getLondenWeather", CowinController.getLondonWeather)
+router.get("/cowin/getWeather",CowinController.getWeather)
+router.post("/cowin/memes",CowinController.memes)
 
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
-
-
 
 module.exports = router;
